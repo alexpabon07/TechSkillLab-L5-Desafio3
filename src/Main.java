3e6ed371a9a413d95ea534ab537d5599880e1832
Alex Pabon <alex312-@hotmail.com>
2025-10-14 17:11:46 -0500

    Ejercicios resueltos taller 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import resources.Empleado;
 
 import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.*;
@@ -8,6 +10,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,7 +40,7 @@ public class Main {
                                         Collectors.counting(),
                                         Long::intValue
                                 )
-                ));
+                        ));
 
 
         //To-do: Mostrar empleados por un consumer: Contratados en determinado mes
@@ -94,34 +97,155 @@ public class Main {
         }
         //4. Consumer - Con stream
         empleados.stream()
-                .filter(e -> e.getFechaIng().getMonth() ==  Month.JANUARY)
+                .filter(e -> e.getFechaIng().getMonth() == Month.JANUARY)
                 .forEach(show);
 
+        //To-do: Solucion taller 3: stream y lambdas
+
+        /* 1. Calcule estadisticas de salario (minimo, maximo, promedio)
+         */
+        System.out.println("Stream y lambdas result: Calcule estadisticas de salario");
+        BigDecimal salarioMinimo = empleados.stream()
+                .map((Empleado::getSalario))
+                .min(BigDecimal::compareTo)
+                .orElse(BigDecimal.ZERO);
+        System.out.println("Salario minimo: " + salarioMinimo);
+
+        System.out.println("Stream y lambdas result: ");
+        BigDecimal salarioMaximo = empleados.stream()
+                .map((Empleado::getSalario))
+                .max(BigDecimal::compareTo)
+                .orElse(BigDecimal.ZERO);
+        System.out.println("Salario maximo: " + salarioMaximo);
+
+        System.out.println("Stream y lambdas result: ");
+        Double salarioPromedio = empleados.stream()
+                .collect(Collectors.averagingDouble(listEmpleados ->
+                        listEmpleados.getSalario().doubleValue()));
+        System.out.println("Salario promedio: " + salarioPromedio);
+
+        /* 2. Agrupe por genero y departamento
+         */
+        System.out.println("Stream y lambdas result: Agrupe por genero y departamento");
+        Map<String, Map<String, List<Empleado>>> generoPorDepartamentos = empleados.stream()
+                .collect(Collectors.groupingBy(Empleado::getGenero,
+                        Collectors.groupingBy(Empleado::getDepartamento)));
+
+        generoPorDepartamentos.forEach((genero, departamento) -> {
+            System.out.println("Genero: " + genero);
+            departamento.forEach((listDepartamentos, listEmpleados) -> {
+                System.out.println("Departamento: " + listDepartamentos);
+                listEmpleados.forEach(System.out::println);
+            });
+        });
+
+        /* 3. Obtenga el top 3 de empleados con mas antiguedad
+         */
+        System.out.println("Stream y lambdas result: top 3 de empleados con mas antiguedad");
+        List<Empleado> listaTopEmpleadosAntiguos = empleados.stream()
+                .filter(empleado -> empleado.getActive().equals(Boolean.TRUE))
+                .sorted(Comparator.comparing(Empleado::getFechaIng))
+                .limit(3)
+                .toList();
+        listaTopEmpleadosAntiguos.forEach(System.out::println);
+
+        /* 4. Genere un mapa Map<String, Double> con el porcentaje de empleados
+        activos por departamento
+         */
+        System.out.println("Stream y lambdas result: porcentaje empleados por departamento");
+        Map<String, Double> porcentajeEmpleadosActivosPorDepartamento = empleados.stream()
+                .collect(Collectors.groupingBy(Empleado::getDepartamento, Collectors.teeing(
+                        Collectors.filtering(Empleado::getActive, Collectors.counting()),
+                        Collectors.counting(),
+                        (activosPorDept, totalPorDepartamento) -> (totalPorDepartamento == 0)
+                                ? 0.0 : (activosPorDept * 100) / totalPorDepartamento)
+                ));
+        porcentajeEmpleadosActivosPorDepartamento.forEach((departamento, porcentaje) ->
+                System.out.println("Departamento: " + departamento + " porcentaje empleados activos: " + porcentaje));
+
+        /* 5. Compare el rendimiento entre .stream() y .parallelStream()
+        (tiempo de ejecucion) genereando una lista de 5 millones de empleados con
+        datos aleatorios. Calcular:
+         */
+        List<String> nombres = List.of("Camila", "Camilo", "Carlos", "Estefanía", "Juan", "José", "Julián", "María", "Raúl");
+        List<String> apellidos = List.of("Flores", "Franco", "Gutierrez", "López", "Rodríguez", "Silva");
+        List<String> generos = List.of("F", "M");
+        List<String> cargos = List.of("Asistente Contable", "Desarrollador", "Tester QA", "Reclutador", "Soporte TI");
+        List<String> departamento = List.of("Contabilidad", "Informática", "Talento Humano");
+
+        System.out.println("Stream y lambdas result: Compare el rendimiento entre .stream() y .parallelStream()");
+        List<Empleado> empleadosGenerados = IntStream.range(0, 5_000_000)
+                .mapToObj(item -> new Empleado(
+                        nombres.get((int) (Math.random() * nombres.size())),
+                        apellidos.get((int) (Math.random() * apellidos.size())),
+                        generos.get((int) (Math.random() * generos.size())),
+                        departamento.get((int) (Math.random() * departamento.size())),
+                        cargos.get((int) (Math.random() * cargos.size())),
+                        BigDecimal.valueOf((int) (Math.random() * 1000) + 300),
+                        LocalDate.now().minusDays((int) (Math.random() * 3650))
+                ))
+                .toList();
+
+        /* a. Salario promedio (usando stream() y parallelStream()
+         */
+        System.out.println("Stream y lambdas result: Salario promedio");
+        Instant inicioSecuencial = Instant.now();
+        Double promedioSecuencial = empleadosGenerados.stream()
+                .mapToDouble(empleado -> empleado.getSalario().doubleValue())
+                .average().orElse(0.0);
+        Instant finalSecuencial = Instant.now();
+        System.out.println("PROMEDIO SECUENCIAL:");
+        System.out.println("Resultado promedio: " + promedioSecuencial + ", tiempo: " +
+                Duration.between(inicioSecuencial, finalSecuencial).abs().toMillis() + "ms");
+
+        System.out.println("PROMEDIO PARALELO:");
+        Instant inicioParalelo = Instant.now();
+        Double promedioParalelo = empleadosGenerados.parallelStream()
+                .mapToDouble(empleado -> empleado.getSalario().doubleValue())
+                .average().orElse(0.0);
+        Instant finalParalelo = Instant.now();
+        System.out.println("Resultado promedio: " + promedioSecuencial + ", tiempo: " +
+                Duration.between(inicioParalelo, finalParalelo).abs().toMillis() + "ms");
+
+        /* b. Cantidad de empleados por departamento (Collectors.groupingBy)
+         */
+        System.out.println("Stream y lambdas result: Cantidad de empleados por departamento");
+        Instant inicioCantidadEmpleadosSecuencial = Instant.now();
+        Map<String, Long> cantidadEmpleadosPorDepartamento = empleadosGenerados.stream()
+                .collect(Collectors.groupingBy(Empleado::getDepartamento,
+                        Collectors.counting()));
+        Instant finalCantidadEmpleadosSecuencial = Instant.now();
+        System.out.println("Cantidad empleados por departamento secuencial: " + cantidadEmpleadosPorDepartamento +
+                ", tiempo: " + Duration.between(inicioCantidadEmpleadosSecuencial, finalCantidadEmpleadosSecuencial).toMillis() + "ms");
+
+        Instant inicioCantidadEmpleadosParalelo = Instant.now();
+        Map<String, Long> cantidadEmpleadosPorDepartamento2 = empleadosGenerados.parallelStream()
+                .collect(Collectors.groupingBy(Empleado::getDepartamento,
+                        Collectors.counting()));
+        Instant finalCantidadEmpleadosParalelo = Instant.now();
+        System.out.println("Cantidad empleados por departamento paralelo: " + cantidadEmpleadosPorDepartamento2 +
+                ", tiempo: " + Duration.between(inicioCantidadEmpleadosParalelo, finalCantidadEmpleadosParalelo).toMillis() + "ms");
     }
 
-    public static void loadEmpleados(List<Empleado> empleadoList){
+    public static void loadEmpleados(List<Empleado> empleadoList) {
         empleadoList.add(new Empleado("María", "Rodríguez", "F", "Contabilidad", "Asistente Contable", new BigDecimal(700), LocalDate.parse("2021-04-01")));
         empleadoList.add(new Empleado("Juan", "Gutierrez", "M", "Talento Humano", "Reclutador", new BigDecimal(500), LocalDate.parse("2023-03-11"), LocalDate.parse("2024-04-01"), false));
-        empleadoList.add(new Empleado("José", "Albornoz", "M","Contabilidad", "Asistente Contable", new BigDecimal(800), LocalDate.parse("2020-08-15"), LocalDate.parse("2023-05-01"), false));
+        empleadoList.add(new Empleado("José", "Albornoz", "M", "Contabilidad", "Asistente Contable", new BigDecimal(800), LocalDate.parse("2020-08-15"), LocalDate.parse("2023-05-01"), false));
         empleadoList.add(new Empleado("Julián", "Flores", "M", "Informática", "Soporte TI", new BigDecimal(800), LocalDate.parse("2023-11-01")));
-        empleadoList.add(new Empleado("Camila", "Mendoza","F", "Informática", "Desarrollador UI/UX", new BigDecimal(1000), LocalDate.parse("2021-07-08")));
+        empleadoList.add(new Empleado("Camila", "Mendoza", "F", "Informática", "Desarrollador UI/UX", new BigDecimal(1000), LocalDate.parse("2021-07-08")));
         empleadoList.add(new Empleado("Camilo", "López", "M", "Contabilidad", "Supervisor Contable", new BigDecimal(1500), LocalDate.parse("2020-04-11")));
         empleadoList.add(new Empleado("Manuel", "Játiva", "M", "Contabilidad", "Asistente Contable", new BigDecimal(850), LocalDate.parse("2023-06-03")));
         empleadoList.add(new Empleado("Carlos", "Franco", "M", "Talento Humano", "Reclutador", new BigDecimal(650), LocalDate.parse("2023-01-07"), LocalDate.parse("2024-12-09"), false));
         empleadoList.add(new Empleado("Raúl", "Echeverría", "M", "Informática", "Infraestructura TI", new BigDecimal(950), LocalDate.parse("2020-02-14")));
         empleadoList.add(new Empleado("Estefanía", "Mendoza", "F", "Talento Humano", "Supervisora TH", new BigDecimal(1600), LocalDate.parse("2021-09-21")));
         empleadoList.add(new Empleado("Julie", "Flores", "F", "Informática", "Desarrollador", new BigDecimal(1200), LocalDate.parse("2021-12-10")));
-        empleadoList.add(new Empleado("Melissa", "Morocho", "F","Contabilidad", "Asistente Contable", new BigDecimal(820), LocalDate.parse("2022-05-22"), LocalDate.parse("2023-07-09"), false));
+        empleadoList.add(new Empleado("Melissa", "Morocho", "F", "Contabilidad", "Asistente Contable", new BigDecimal(820), LocalDate.parse("2022-05-22"), LocalDate.parse("2023-07-09"), false));
         empleadoList.add(new Empleado("Camila", "Mendez", "F", "Contabilidad", "Asistente Cuentas", new BigDecimal(860), LocalDate.parse("2020-10-01")));
-        empleadoList.add(new Empleado("José", "Rodríguez", "M","Informática", "Tester QA", new BigDecimal(1100), LocalDate.parse("2021-10-01")));
-        empleadoList.add(new Empleado("Esteban", "Gutierrez","M", "Talento Humano", "Reclutador", new BigDecimal(700), LocalDate.parse("2023-04-01")));
-        empleadoList.add(new Empleado("María", "López","F", "Contabilidad", "Asistente Contable", new BigDecimal(840), LocalDate.parse("2020-02-20"), LocalDate.parse("2024-07-15"), false));
-        empleadoList.add(new Empleado("Cecilia", "Marín","F", "Informática", "Supervisora TI", new BigDecimal(2000), LocalDate.parse("2020-04-21")));
-        empleadoList.add(new Empleado("Edison", "Cáceres","M", "Informática", "Desarrollador TI", new BigDecimal(1300), LocalDate.parse("2023-07-07")));
-        empleadoList.add(new Empleado("María", "Silva", "F","Contabilidad", "Asistente Contable", new BigDecimal(900), LocalDate.parse("2021-11-15"), LocalDate.parse("2022-08-09"), false));
-
+        empleadoList.add(new Empleado("José", "Rodríguez", "M", "Informática", "Tester QA", new BigDecimal(1100), LocalDate.parse("2021-10-01")));
+        empleadoList.add(new Empleado("Esteban", "Gutierrez", "M", "Talento Humano", "Reclutador", new BigDecimal(700), LocalDate.parse("2023-04-01")));
+        empleadoList.add(new Empleado("María", "López", "F", "Contabilidad", "Asistente Contable", new BigDecimal(840), LocalDate.parse("2020-02-20"), LocalDate.parse("2024-07-15"), false));
+        empleadoList.add(new Empleado("Cecilia", "Marín", "F", "Informática", "Supervisora TI", new BigDecimal(2000), LocalDate.parse("2020-04-21")));
+        empleadoList.add(new Empleado("Edison", "Cáceres", "M", "Informática", "Desarrollador TI", new BigDecimal(1300), LocalDate.parse("2023-07-07")));
+        empleadoList.add(new Empleado("María", "Silva", "F", "Contabilidad", "Asistente Contable", new BigDecimal(900), LocalDate.parse("2021-11-15"), LocalDate.parse("2022-08-09"), false));
     }
-
-
-
 }
